0e7d98328ff30cc6663da68eb1068a0f18b5fd67
Lyndogwin <brand949@gmail.com>
2019-03-26 17:22:53 +0000

    added redirect for manager that is already logged

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,10 +1,14 @@
 class SessionsController < ApplicationController
   def new
+    if !current_manager.nil?
+      redirect_to Manager.find_by(username: current_manager.username)
+    end
   end
   
   def create
     manager = Manager.find_by(username: session_params[:username])
     if session_params[:password] == manager.password
+      login manager
       redirect_to manager
     else
       render 'new'
@@ -12,6 +16,7 @@ class SessionsController < ApplicationController
   end
   
   def destroy
+    log_out
   end
 end
 
--- app/helpers/sessions_helper.rb
+++ app/helpers/sessions_helper.rb
@@ -1,2 +1,17 @@
 module SessionsHelper
+    def login(manager)
+        session[:manager_id] = manager.id
+    end
+    
+    def current_manager
+        if session[:manager_id]
+            @current_manager ||= Manager.find_by(id: session[:manager_id])
+        end
+    end
+    
+    def log_out
+        session.delete(:manager_id)
+        @current_manager = nil
+        redirect_to welcome_index_path
+    end
 end
