9aab50c3411dc73247679fb2dcf8f5cb98b60fcd
Lyndogwin <brand949@gmail.com>
2019-03-27 16:57:43 +0000

    Minor changes from last commit

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,23 +1,22 @@
 class SessionsController < ApplicationController
   def new
-    if !current_manager.nil?
+    if !current_manager.nil? # defined in session helper
       redirect_to Manager.find_by(id: session[:manager_id])
     end
   end
   
   def create
-    manager = Manager.find_by(username: session_params[:username])
-    if manager && manager.authenticate(session_params[:password])
-      log_in manager
-      redirect_to manager
+    @manager = Manager.find_by(username: session_params[:username])
+    if @manager && @manager.authenticate(session_params[:password])
+      log_in @manager # defined in session helper
+      redirect_to @manager
     else
       render 'new'
     end
   end
   
   def destroy
-    # see session helper
-    log_out
+    log_out # defined in session helper
     redirect_to welcome_index_path
   end
 end
