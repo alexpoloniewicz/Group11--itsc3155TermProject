8b37456b53d6c65edb0fd6549407f5ca57bd65dd
Lyndogwin <brand949@gmail.com>
2019-03-30 17:46:48 +0000

    added more links to nav bar

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,7 @@
 Rails.application.routes.draw do
   get 'sessions/new'
   get 'welcome/index'
+  get 'managers/index'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   
   
