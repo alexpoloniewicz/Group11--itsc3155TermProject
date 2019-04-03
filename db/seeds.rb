425f6f6d8eeda30504a124fb638b025363caa66b
Lyndogwin <brand949@gmail.com>
2019-04-03 20:30:04 +0000

    updated seeds.rb

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,12 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+Manager.create!(username: "SoviMaster", password: "godchef", password_confirmation: "godchef")
+FoodClass.create!(name: "cereal")
+FoodClass.create!(name: "entree")
+FoodClass.create!(name: "snack")
+Food.create!(name: "friut loops", tod: "breakfast", FoodClass_id: 1, likes: 20, dislikes: 5)
+Food.create!(name: "captain krunch", tod: "breakfast", FoodClass_id: 1, likes: 12, dislikes: 5)
+Food.create!(name: "eggs", tod: "breakfast", FoodClass_id: 2, likes: 15, dislikes: 20)
+Food.create!(name: "pizza", tod: "breakfast", FoodClass_id: 2, likes: 50, dislikes: 7)
+Food.create!(name: "friut loops", tod: "breakfast", FoodClass_id: 1, likes: 20, dislikes: 5)
