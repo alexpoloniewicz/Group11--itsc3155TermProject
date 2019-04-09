625202d34bd6aa173f47dca547da3b0032004f96
Lyndogwin <brand949@gmail.com>
2019-04-09 22:04:25 +0000

    added doc to food.rb

--- app/models/food.rb
+++ app/models/food.rb
@@ -1,3 +1,5 @@
 class Food < ApplicationRecord
-    has_many :reviews, dependent: :destroy
+    # if food item is removed form the db,
+    # this line will ensure that the related reviews will be deleted as well.
+    has_many :reviews, dependent: :destroy 
 end
