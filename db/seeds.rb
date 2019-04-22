d59d6ead2cc1d3acba29f7fba26b03af487143d2
Lyndogwin <brand949@gmail.com>
2019-04-22 23:17:29 +0000

    Updated database seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,6 +5,16 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+# rails db:reset db:mirgrate ----> to refesh databases
+preselect = ["Flawless!",
+              "Too much seasoning",
+              "Too bland",
+              "Served too often",
+              "Temperature was off",
+              "Seemed unsanitary",
+              "Not served often enough",
+              "My favorite dish",
+              "Perfect amount of seasoning"]
 Manager.create!(username: "SoviMaster", password: "godchef", password_confirmation: "godchef")
 FoodClass.create!(name: "Cereal")
 FoodClass.create!(name: "Entree")
@@ -14,9 +24,11 @@ Food.create!(name: "friut loops", tod: "breakfast", FoodClass_id: 1, likes: 20, 
 Food.create!(name: "captain krunch", tod: "breakfast", FoodClass_id: 1, likes: 12, dislikes: 5)
 Food.create!(name: "eggs", tod: "breakfast", FoodClass_id: 2, likes: 15, dislikes: 20)
 Food.create!(name: "pizza", tod: "lunch", FoodClass_id: 2, likes: 50, dislikes: 7)
-Food.create!(name: "potato wedges", tod: "lunch", FoodClass_id: 4, likes: 20, dislikes: 5)
-Food.create!(name: "chicken alfredo", tod: "lunch", FoodClass_id: 2, likes: 20, dislikes: 5)
-Review.create!(email: "brand949@gmail.com", Food_id: 1, like: 1, dislike: 0, comment: preselect[0])
-Review.create!(email: "brand949@gmail.com", Food_id: 4, like: 0, dislike: 1, comment: preselect[4])
-Review.create!(email: "brand949@gmail.com", Food_id: 3, like: 1, dislike: 0, comment: preselect[3])
-Review.create!(email: "brand949@gmail.com", Food_id: 6, like: 1, dislike: 0, comment: preselect[2])
+Food.create!(name: "potato wedges", tod: "dinner", FoodClass_id: 4, likes: 20, dislikes: 5)
+Food.create!(name: "chicken alfredo", tod: "dinner", FoodClass_id: 2, likes: 20, dislikes: 5)
+Review.create!(email: "brand949@gmail.com", food_id: 1, like: 1, dislike: 0, comment: preselect[0])
+Review.create!(email: "brand949@gmail.com", food_id: 4, like: 0, dislike: 1, comment: preselect[2])
+Review.create!(email: "brand949@gmail.com", food_id: 3, like: 1, dislike: 0, comment: preselect[3])
+Review.create!(email: "brand949@gmail.com", food_id: 6, like: 1, dislike: 0, comment: preselect[7])
+Review.create!(email: "brand949@gmail.com", food_id: 2, like: 0, dislike: 1, comment: preselect[5])
+Review.create!(email: "brand949@gmail.com", food_id: 5, like: 1, dislike: 0, comment: preselect[6])
